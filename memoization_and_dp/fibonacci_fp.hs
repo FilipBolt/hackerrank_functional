eddbda2f56f00d6d2fd1fae96e43a6a149dc8545
Filip Boltuzic <filipbolt@gmail.com>
2018-03-07 17:47:09 +0100

    Moving to correct (memoization) folder

---
+++ memoization_and_dp/fibonacci_fp.hs
@@ -0,0 +1,30 @@
+main :: IO()
+main = do
+    n_temp <- getLine
+    let n = read n_temp :: Int
+    targets <- readLines n
+    let t = divide $ map (fibos !!) targets
+    putStr $ specialPrint t
+    return ()
+
+readLines :: Int -> IO [Int] 
+readLines n
+    | n <= 0 = return []
+    | otherwise = do
+        x <- getLine
+        let el = read x :: Int
+        xs <- readLines (n - 1)
+        let ret = el : xs
+        return ret
+
+
+fibos :: [Integer]
+fibos = serie 0 1
+    where serie i j = i : serie j (i+j)
+
+
+divide :: [Integer] -> [Integer]
+divide = map (`mod` (100000000 + 7))    
+
+specialPrint [] = []
+specialPrint (x:xs) = show x ++ "\n" ++ specialPrint xs
